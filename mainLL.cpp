ca8afccbcd4124822dcb42167498c795264b3da1
JuukyoKai <juukyokai00@gmail.com>
2020-02-18 10:52:16 +0700

    update Linked List

--- mainLL.cpp
+++ mainLL.cpp
@@ -94,6 +94,10 @@ class SingleLL{
 			}else if(pPre==NULL){
 				temp->next=head;
 				head=temp;
+			}else if(pCur->next==NULL){
+				tail=temp;
+				temp->next = pCur->next;
+				pCur->next = temp;
 			}else{
 				temp->next = pCur->next;
 				pCur->next = temp;
@@ -141,6 +145,9 @@ class SingleLL{
 			
 			if(pCur==NULL){
 				cout << "Maaf Data tidak ditemukan";
+			}else if(pCur->next==NULL){
+				tail=pPre;
+				delNode(pPre,pCur);
 			}else{
 				delNode(pPre,pCur);
 				return hasil;
@@ -159,6 +166,9 @@ class SingleLL{
 			
 			if(pCur==NULL){
 				cout << "Maaf data tidak ditemukan!";
+			}else if(pCur->next==NULL){
+				tail=pPre;
+				delNode(pPre,pCur);
 			}else{
 				delNode(pPre,pCur);
 			}
@@ -175,10 +185,11 @@ int main() {
 	string cari;
 	string del;
 	string up;
-	
-	
-	
 	SingleLL sll;
+	bool loop=true;
+	char input;
+	
+	
 	Node *simpul1= new Node("Atika",NULL);
 	Node *simpul2= new Node("Ben",NULL);
 	Node *simpul3= new Node("Chika",NULL);
@@ -187,44 +198,78 @@ int main() {
 	simpul2->next = simpul3;
 	sll.head=simpul1;
 	sll.tail=simpul3;
-	//checking simple data
-	sll.printList();
-	cout << "Last Node : " << sll.last() << endl;
-	getch();
+	
+	
+	while(loop==true){
+		system("cls");
+		cout << "1. Check Simple Data\n";
+		cout << "2. Append New Data\n";
+		cout << "3. Prepend New Data\n";
+		cout << "4. Insert New Data\n";
+		cout << "5. Delete Data\n";
+		cout << "6. Update Data\n";
+		cout << "7. Extract First Data\n";
+		cout << "8. Extract Data\n";
+		cout << "press q to quit!!\n";
+		cout << "Masukkan menu : ";
+		cin >> input;
+		
+		if(input=='1'){
+			//checking simple data
+			sll.printList();
+			cout << "Last Node : " << sll.last() << endl;
+			getch();	
+		}else if(input=='2'){
+			//Input last data
+			sll.append("Delta");
+			sll.printList();
+			cout << "Last Node : " << sll.last() << endl;
+			getch();
+		}else if(input=='3'){
+			//input first data
+			sll.prepend("Alpha");
+			sll.printList();
+			cout << "First Node : " << sll.first() <<endl;
+			getch();
+		}else if(input=='4'){
+			//input mid data
+			sll.printList();
+			cout << "Masukkan data setelah Nama : ";
+			cin >> cari;
+			sll.insertMid("Gamma",cari);	
+			sll.printList();
+			getch();
+		}else if(input=='5'){
+			//Delete data
+			sll.printList();
+			cout << "Masukkan data yang ingin dihapus : ";
+			cin >> del;
+			sll.deleteData(del);
+			sll.printList();
+			getch();
+		}else if(input=='6'){
+			//update data
+			sll.printList();
+			cout << "Masukkan data yang ingin diganti : ";
+			cin >> up;
+			sll.updateData(up);
+			sll.printList();
+		}else if(input=='7'){
+			sll.printList();
+			sll.first();
+			cout << "Node yang terekstrak adalah : " << sll.extractFirst();
+			sll.printList();
+		}else if(input=='8'){
+			cout << "Masukkan data yang ingin diekstrak : ";
+			cin >> del;
+			cout << "data yang di ekstrak adalah : " << sll.extract(del);
+			sll.printList();
+		}else if(input=='q'){
+			loop=false;
+		}
+	}
 	system("cls");
-	//Input last data
-	sll.append("Delta");
-	sll.printList();
-	cout << "Last Node : " << sll.last() << endl;
-	getch();
-	system("cls");
-	//input first data
-	sll.prepend("Alpha");
-	sll.printList();
-	cout << "First Node : " << sll.first() <<endl;
-	getch();
-	system("cls");
-	//input mid data
-	sll.printList();
-	cout << "Masukkan data setelah Nama : ";
-	cin >> cari;
-	sll.insertMid("Gamma",cari);	
-	sll.printList();
-	getch();
-	system("cls");
-	//Delete data
-	sll.printList();
-	cout << "Masukkan data yang ingin dihapus : ";
-	cin >> del;
-	sll.deleteData(del);
-	sll.printList();
-	getch();
-	system("cls");
-	//update data
-	sll.printList();
-	cout << "Masukkan data yang ingin diganti : ";
-	cin >> up;
-	sll.updateData(up);
-	sll.printList();
+	cout<<"Terima Kasih!!";
+	
 	return 0;
 }
